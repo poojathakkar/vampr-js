3b63fe9bc57dfbc9acb0915dcf8d4d4a1345551e
Pooja Thakkar <poojapthakkar@gmail.com>
2020-07-11 17:41:54 +0000

    original

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
 
+    //climb up tree using iteration until no creator is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);    
   }
 
   /** Stretch **/
@@ -36,8 +45,13 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    if (this.isMoreSeniorThan(vampire)) {
+      return this;
+    } else {
+      return vampire;
+    }
   }
+
 }
 
 module.exports = Vampire;
