b2f3526c6ae49a84e7d7afb831ec054fdaaae315
poojathakkar <poojapthakkar@gmail.com>
2020-07-12 22:02:00 -0400

    Update vampire.js

--- vampire.js
+++ vampire.js
@@ -10,25 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
 
+    //climb up tree using iteration until no creator is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);    
   }
 
-  /** Stretch **/
+    /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
   // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
@@ -36,7 +45,51 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return this;
+    } else {
+      return vampire;
+    }
+  }
+   
+  /** Tree traversal methods **/
 
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    for (const offspringNode of this.offspring) {
+      if (offspringNode.vampireWithName(name)) {
+        return offspringNode.vampireWithName(name);
+      };
+    }
+    return null;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let totalDescendents = this.numberOfOffspring; // 1
+    
+    // Use depth first traversal to calculate the total employees
+    for (const offspringNode of this.offspring) {
+      totalDescendents += offspringNode.totalDescendents ;
+    }
+    return totalDescendents;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millennials = [];
+
+    if (this.yearConverted > 1980) {
+      millennials.push(this);
+    }
+    for (const offspringNode of this.offspring) {
+      const millennialAfter = offspringNode.allMillennialVampires; 
+      millennials = millennials.concat(millennialAfter);
+    }
+    return millennials;
   }
 }
 
